088c822e6e74994d380b0d065e7c41ebcf639443
Сергей Владимирович <kamaz2106@gmail.com>
2021-07-23 13:58:50 +0400

    footer fix

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -1,88 +1,78 @@
 import React from "react";
-import g from "styles/Main.module.scss";
 import { MainLogo } from "components/SVG/MainLogo";
-
 import Link from "next/link";
-import clsx from "clsx";
 import { AsapLogoSvg } from "components/SVG/AsapLogoSVG";
+import { Wrapper } from "components/layout/Wrapper/Wrapper";
+import { formatPhone } from "functions.js";
 import s from "./Footer.module.scss";
 
+const footerLinks = [
+  { title: "Доставка", link: "/delivery" },
+  { title: "Продукция", link: "/products" },
+  { title: "О нас", link: "/farm" },
+  { title: "Точки продаж", link: "/sale-points" },
+  { title: "Рецепты", link: "/articles" }
+];
+
 export const Footer = () => (
-  <footer className={clsx(s.footer)}>
-    <div className={clsx(g.wrapper)}>
-      <div className={clsx(s.container)}>
-        <div className={clsx(s.block)}>
-          <nav className={clsx(s.menu)}>
-            <h2 className={clsx(s.link)}>
-              <a href="/">Магазин</a>
-            </h2>
-            <h2 className={clsx(s.link)}>
-              <a href="/">Доставка</a>
-            </h2>
-            <h2 className={clsx(s.link)}>
-              <a href="/">О нас</a>
-            </h2>
-            <h2 className={clsx(s.link)}>
-              <a href="/">Продукция</a>
-            </h2>
-            <h2 className={clsx(s.link)}>
-              <a href="/">Рецепты</a>
-            </h2>
+  <footer className={s.footer}>
+    <Wrapper>
+      <div className={s.container}>
+        <div className={s.block}>
+          <nav className={s.menu}>
+            {footerLinks.map(({ title, link }) =>
+              (
+                <Link href={link} key={link}>
+                  <a className={s.link}>
+                    <h2 className={s.title}>{title}</h2>
+                  </a>
+                </Link>
+              )
+            )}
           </nav>
-          <a href="mailto:" className={clsx(s.contact)}>
+          <a href="mailto:" className={s.contact}>
             Написать нам
           </a>
         </div>
-        <div className={clsx(s.block)}>
-          <nav className={clsx(s.menu)}>
-            <h3 className={clsx(s.link)}>
-              <a href="/">Точки продаж</a>
-            </h3>
-            <h3 className={clsx(s.link)}>
-              <a href="/">Контакты</a>
-            </h3>
-            <h3 className={clsx(s.link, s.social)}>
-              <a href="/">Instagram</a>
-            </h3>
-            <h3 className={clsx(s.link, s.social)}>
-              <a href="/">Facebook</a>
-            </h3>
+        <div className={s.block}>
+          <nav className={s.menu}>
+            <a className={s.link} href="https://instagram.com/instagram"><h3 className={s.title}>Instagram</h3></a>
+            <a className={s.link} href="https://facebook.com"><h3 className={s.title}>Facebook</h3></a>
           </nav>
-          <div className={clsx(s.info)}>
-            <address className={clsx(s.address)}>
+          <div className={s.info}>
+            <address className={s.address}>
               Смоленская область, Кардымовский район, село Каменка ООО "Красная горка", ОГРН 1136733008763, ИНН
               6722041039
             </address>
-            <div className={clsx(s.contacts)}>
-              <a href="tel:" className={clsx(s.contact)}>
-                8 (800) 250-58-24
-              </a>
-              <a href="mailto:" className={clsx(s.contact)}>
-                sales@koico.ru
-              </a>
+            <div className={s.contacts}>
+              <a href={`tel:${formatPhone("8 (800) 250-58-24")}`} className={s.contact}>8 (800) 250-58-24</a>
+              <a href="mailto:sales@koico.ru" className={s.contact}>sales@koico.ru</a>
             </div>
-            <a
-              href="https://asap-ag.ru"
-              target="_blank"
-              className={clsx(s.asap)} rel="noreferrer"
-            >
-              <AsapLogoSvg/>
-            </a>
+
           </div>
         </div>
       </div>
-      <div className={clsx(s.corp)}>
+      <div className={s.corp}>
         {
           <Link href="/">
-            <a>
+            <a className={s.logo}>
               <MainLogo/>
             </a>
           </Link>
         }
-        <span>© Pty Ltd and Wild Game Resources</span>
-        <span>Australia Pty Ltd</span>
-        <span>Terms & Conditions</span>
+        <div>
+          <span>© Pty Ltd and Wild Game Resources</span>
+          <span>Australia Pty Ltd</span>
+          <span>Terms & Conditions</span>
+        </div>
+        <a
+          href="https://asap-ag.ru"
+          target="_blank"
+          className={s.asap} rel="noreferrer"
+        >
+          <AsapLogoSvg/>
+        </a>
       </div>
-    </div>
+    </Wrapper>
   </footer>
 );
