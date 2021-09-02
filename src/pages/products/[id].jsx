66a8bf6f67499988e87dc0c6ccafa923b443e4a4
Сергей Владимирович <kamaz2106@gmail.com>
2021-09-02 18:39:00 +0400

    Update [id].jsx

--- src/pages/products/[id].jsx
+++ src/pages/products/[id].jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { DescriptionSection } from 'components/sections/card/DescriptionSection/DescriptionSection';
+import React from "react";
+import { DescriptionSection } from "components/sections/card/DescriptionSection/DescriptionSection";
 
-import { Slider } from 'components/common/Slider/Slider';
-import { Product } from 'components/common/Product/Product';
+import { Slider } from "components/common/Slider/Slider";
+import { Product } from "components/Product/Product";
 
 import ProductsAPI from 'api/ProductsAPI';
 
